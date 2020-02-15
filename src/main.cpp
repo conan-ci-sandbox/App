d2873cb78211073cf0826345d51d15dc449c0b5c
czoido <mrgalleta@gmail.com>
2020-02-15 12:29:48 +0100

    use libC

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 #include "libB/libB.h"
+#include "libC/libC.h"
 
 int main() {
     std::cout << "App: " << std::endl;
     hello_libB(1, "called from App");
+    hello_libC(1, "called from App");
     return 0;
 }
